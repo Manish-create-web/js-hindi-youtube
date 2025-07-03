ad10ff690e444dc4479ae5c3aae7a73b1535c076
Manish Jha <mjha22238@gmail.com>
2025-07-03 16:22:14 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,45 @@
+// primitive
+
+// 7 types :string, number, boolean, null, undefined, symbol, bigint.
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedIn =false
+const outSideTemp = null
+let userEmail;
+
+const id = symbol('423')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+//const brighterNumber =2352636276247378n
+
+
+// reference (non primitive)
+
+// array, objects, functions
+
+
+const heroes =  ["shaktiman", "naagraj", "doga"]
+{
+    name:manish;
+
+}
+    
+    
+
+
+
+const myFunction = function()   { 
+console.log("hello world");
+    }
+console.log(typeof bignumber);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
